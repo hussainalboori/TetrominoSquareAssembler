4ab7f068369048d4f699585d51d5dec9d9b4c7f4
hussain alboori <34797449+hussainalboori@users.noreply.github.com>
2023-10-30 15:24:42 +0300

    idk

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"log"
 	"os"
+	"tetris-optimizer/Validation"
 	"time"
 )
 
 func main() {
 	startTime := time.Now()
 	Inpout := os.Args[1:]
-
-	
+	if !validation.CheckIfValid(Inpout){
+		log.Panicln("Error")
+		return
+	}
 }
